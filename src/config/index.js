3e37593d8975df8d236893595377f5105ec17dac
mtrentman <mike.trentman@alveohealth.com>
2017-11-29 14:46:47 -0500

    updated config settings.

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 export default {
   "port": 3005,
-  "enrollmentDatasource": "postgres://socrates:470bCd3M3Al0p3C3Ath3ns!@enrollment.cratp0nk0qj4.us-east-1.rds.amazonaws.com:5432/enrollment",
-  "adminDatasource": "postgres://aquinas:1225aDrOcCaSeCcAiTaLy!@admin.cratp0nk0qj4.us-east-1.rds.amazonaws.com:5432/administration",
+  "enrollmentDatasource": "postgres://user:password@enrollment.cratp0nk0qj4.us-east-1.rds.amazonaws.com:5432/enrollment",
+  "adminDatasource": "postgres://user:password@admin.cratp0nk0qj4.us-east-1.rds.amazonaws.com:5432/administration",
   "bodyLimit": "100kb"
 }
